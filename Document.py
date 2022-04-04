0ab8b45e9b3e7d385182dcb3cc690539fdd490e1
MJS <platut@naver.com>
2022-04-05 00:52:32 +0900

    test

---
+++ Document.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("서소문 질의사항")
---
+++ ETC.py
@@ -0,0 +1,3 @@
+# PS C:\Users\CTO\Desktop\TestPY> git add . --> 2두번째 푸쉬할 경우
+# PS C:\Users\CTO\Desktop\TestPY> git commit -m "Test" --> m 'Message의 약자'
+# PS C:\Users\CTO\Desktop\TestPY> git push origin master --> 깃에 푸쉬한다.
