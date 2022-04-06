e49637febba35f65b8168992275c96deab28b2cb
MJS <platut@naver.com>
2022-04-06 23:39:42 +0900

    e

--- Document.py
+++ Document.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title("서소문 질의사항")
 st.subheader("코어부분 Wall Girder 확인요청")
+st.write("""
+# 테스트 텍스트 아우터리스트
+""")
--- ETC.py
+++ ETC.py
@@ -1,3 +1,6 @@
 # PS C:\Users\CTO\Desktop\TestPY> git add . --> 2두번째 푸쉬할 경우
 # PS C:\Users\CTO\Desktop\TestPY> git commit -m "Test" --> m 'Message의 약자'
 # PS C:\Users\CTO\Desktop\TestPY> git push origin master --> 깃에 푸쉬한다.
+
+
+# https://share.streamlit.io/meejinsee/ssm/Document.py
