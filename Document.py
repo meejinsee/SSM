840867c85b6b5ebd666439f29a98ecd18106f507
MJS <platut@naver.com>
2022-04-07 00:41:13 +0900

    e

--- Document.py
+++ Document.py
@@ -1,4 +1,16 @@
 import streamlit as st
 import pandas as pd
+import pyodbc as py
 
 st.title("서소문 질의사항")
+# conn = py.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Ron\Desktop\Test\test_database.accdb;')
+# cursor = conn.cursor()
+# cursor.execute('select * from products')
+   
+# for row in cursor.fetchall():
+#     print (row)
+
+a = []
+
+
+
